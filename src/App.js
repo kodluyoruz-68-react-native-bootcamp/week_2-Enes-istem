21ce10fb5be2e85aa5d67586778677b4afa0f194
Enes iştem <enestm93@gmail.com>
2020-12-17 23:43:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useState,useEffect} from 'react';
+import {SafeAreaView, View, Text, FlatList, TouchableOpacity, Alert} from 'react-native';
+import Header from './components/Header';
+import Combine from './components/Combine'
+import TodoCard from './components/TodoCard'
+
 
 /**
  * TextInput: testID="input" (component which is user types the todo text)
@@ -8,9 +12,82 @@ import {SafeAreaView, View, Text} from 'react-native';
  */
 
 function App() {
+  const textInput = React.useRef();
+  const clearInput = () => (textInput.current.clear());
+
+  const [condition, setCondition] = useState([]);
+  const [text, setText] = useState("");
+  const [counter,setCounter] = useState(0);
+
+  const createTodo = (text) => {
+    if(text) {
+    const Element = {
+      id: Math.random().toString(),
+      todo: text,
+      isDone: false
+    }
+    let newList = [Element, ...condition]
+    setCondition(newList);
+    setCounter(newList.length);
+    }
+    clearInput()
+    // setText((e) => e.target.clear())
+  }
+
+  const countToDo = () =>{
+    let newList = [...condition];
+    const count = newList.filter(i => i.isDone ==false)
+    setCounter(count.length)
+    // console.log(count.length)
+  }
+
+  const disabledFunc = (todoId) => {
+      let newList = [...condition];
+      const todoIndex = newList.findIndex(i => i.id == todoId);
+      newList[todoIndex].isDone = !newList[todoIndex].isDone
+      setCondition(newList)
+      countToDo()
+      // Alert.alert("id", todoIndex)
+  }
+
+  const removedFunc = (todoId) => {
+    let newList = [...condition];
+    const removeIndex = newList.findIndex(i => i.id == todoId);
+    newList.splice(removeIndex,1)
+    setCondition(newList)
+    setCounter(newList.length)
+    // countToDo()
+  }
+
+  const renderData = ({item}) => {
+    if(item.todo){
+      return <TodoCard item={item} onDone={()=> disabledFunc(item.id)} onLong ={()=> removedFunc(item.id)}/>
+    }
+    
+  }
+ 
   return (
     // ...
-    null
+    <SafeAreaView style={{flex:1, backgroundColor: '#bdbdbd'}}>
+      <View style={{flex:1}}>
+        <Header condition= {condition} counter = {counter} />
+        {/* <Text>{text}</Text> */}
+      
+        < FlatList
+        testID="list"
+        keyExtractor={(_,index) => index.toString()}
+        data = {condition}
+        renderItem = {renderData}
+        ListEmptyComponent={() => <View style = {{alignItems:'center', justifyContent:'center'}}><Text style={{fontSize:25}}>Nothing to do ...</Text></View>}
+        />
+        <Combine placeholder="Please enter your todo" 
+        onChanging= {(text) => setText(text)} 
+        onSubmit={() => createTodo(text)} 
+        clean={textInput}
+        />
+
+      </View>
+    </SafeAreaView>
   );
 }
 
